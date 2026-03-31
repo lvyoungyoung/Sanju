698b74e509d626488226299296c2453ea2ab73d0
Yang.lv <1057251634@qq,com>
2026-03-31 16:38:15 +0800

    Initial Commit

---
+++ 三句/__App.swift
@@ -0,0 +1,17 @@
+//
+//  __App.swift
+//  三句
+//
+//  Created by 吕扬 on 2026/3/31.
+//
+
+import SwiftUI
+
+@main
+struct __App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
